9ce58da47278dad5cd9a90908ce1ea3ec3998e7a
buraaksen <buraaksen@outlook.com>
2026-02-27 15:13:08 +0200

    Initial

---
+++ src/model/Country.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum Country {
+	Latvia, Italy, Spain, Lithuania, Turkey, other, unkown
+}
---
+++ src/model/Student.java
@@ -0,0 +1,61 @@
+package model;
+
+public class Student {
+	//1. variables
+		private String matriculaNumber;
+		private String name;
+		private String surname;
+		private String faculty;
+		private int birthYear;
+		private Country country;
+		private String passportNumber;
+		
+		//2. getters
+		public String getMatriculaNumber() {
+			return matriculaNumber;
+		}
+		public String getName() {
+			return name;
+		}
+		public String getSurname(){
+			return surname;
+		}
+		public String getFaculty() {
+			return faculty;
+		}
+		public int getBirthYear() {
+			return birthYear;
+		}
+		public Country getCountry() {
+			return country;
+		}
+		public String getPassportNumber() {
+			return passportNumber;
+		}
+		//3. setters
+		
+		
+		public void setMatriculaNumber(String matriculaNumber) {
+			this.matriculaNumber = matriculaNumber;
+		}
+		public void setName(String name) {
+			this.name= name;
+		}
+		private void setFaculty(String Fculty) {
+			this.faculty = faculty;
+		}
+		private void setBirthYear(int birthYear) {
+			this.birthYear = birthYear;
+		}
+		private void setCountry(Country country) {
+			this.country = country;
+		}
+		private void setPassportNumber(String passportNumber) {
+			this.passportNumber = passportNumber;
+		}
+		
+		//4. nor-arg constructor
+		//5. arg-constructor
+		//6. toString
+		//7. other functions 
+}
